32c24e19653f7b9d986eaa375bf5a57a804c62d3
espinozanelson918-arch <espinozanelson918@gmail.com>
2025-10-07 21:37:02 -0600

    hello

--- backend/index.ts
+++ backend/index.ts
@@ -1,13 +1,17 @@
 import express from "express";
-import productoRoutes from "../backend/routes/productRoutes";
-import usuarioRoutes from "../backend/routes/usuarioRoutes";
-import { VercelRequest, VercelResponse } from '@vercel/node';
+import productoRoutes from "./routes/productRoutes";
+import usuarioRoutes from "./routes/usuarioRoutes";
 
 const app = express();
+const PORT = 3000;
+
+// Middleware para trabajar con JSON
 app.use(express.json());
 app.use("/productos", productoRoutes);
 app.use("/usuarios", usuarioRoutes);
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  app(req, res);
-}
+
+// Iniciar servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
