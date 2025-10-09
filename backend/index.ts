59727e9ca431767bea6089f6cf0b369a5a0ad3cf
espinozanelson918-arch <espinozanelson918@gmail.com>
2025-10-09 01:10:32 -0600

    prueba de commit

--- backend/index.ts
+++ backend/index.ts
@@ -1,19 +1,29 @@
 import express from "express";
+import path from "path";
 import productoRoutes from "./routes/productRoutes";
 import usuarioRoutes from "./routes/usuarioRoutes";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Middleware para trabajar con JSON
+// Middleware para JSON
 app.use(express.json());
-app.use("/productos", productoRoutes);
-app.use("/usuarios", usuarioRoutes);
 
+// Rutas API
+app.use("/api/productos", productoRoutes);
+app.use("/api/usuarios", usuarioRoutes);
+
+// Servir frontend
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+// Redirigir cualquier ruta no API a index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/index.html"));
+});
 
 // Iniciar servidor
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
 
-
+export default app; // importante para Vercel
